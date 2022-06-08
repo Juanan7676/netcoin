3a804063841ab2f48a335e354437f5f51bc8a469
Juanan7676 <juan4999@gmail.com>
2022-06-08 21:53:03 +0200

    Fix tests again

--- tests/lib/mocks/component.lua
+++ tests/lib/mocks/component.lua
@@ -3,6 +3,8 @@ local sha = require("sha2")
 local component = {}
 
 component.data = {}
+component.modem = {}
+component.modem.address = "testing"
 
 function component.data.sha256(str)
     return sha.hex_to_bin(sha.sha256(str))
